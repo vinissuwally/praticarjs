c2c720b22f68e6d9dbad2b2e668b4d7220544e79
spark.zz <vinynine2009@gmail.com>
2026-09-04 11:01:47 -0300

    operadores matematicos

--- scripts/idex.js
+++ scripts/idex.js
@@ -1,6 +1,9 @@
-var a = 1;
-var b = 5;
-var c = a + b; 
-alert(`${c}`)
-/* definindo variaveis e printando elas com alert, podendo 
-utlizar outras operações tambem !*/
+// operadores aritmeticos (matematicos)
+
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 2;
+total =++valor1;
+// usando operador aritmetico e de atribuição
+alert(total);
+alert("o valor 1 ficou: " + valor1)
