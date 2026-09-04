5467b27471d809d6db4b8ed415b693cc3f70edc1
spark.zz <vinynine2009@gmail.com>
2026-09-04 11:50:55 -0300

    operadores comparativos

--- scripts/idex.js
+++ scripts/idex.js
@@ -1,9 +1,9 @@
-// operadores de sequência 
+// operadores de comparação 
 
 var valor1, valor2, total;
-valor1 = "viny ";
-valor2 = "tibe";
+valor1 = 8;
+valor2 = 12; // == ver oq tem dentro das aspas já o === não
 
-total = valor1 += valor2;
+total = (valor1 >= 9 ); // falsa pois não é o mesmo numero  
 
 alert(total);
