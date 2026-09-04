a424505adbc396539398d4af8e4e35422d46da26
spark.zz <vinynine2009@gmail.com>
2026-09-03 21:19:41 -0300

    vendo como funciona var com numeros

--- scripts/idex.js
+++ scripts/idex.js
@@ -1,6 +1,14 @@
-var a = 1;
-var b = 5;
-var c = a + b; 
-alert(`${c}`)
-/* definindo variaveis e printando elas com alert, podendo 
-utlizar outras operações tambem !*/
+/*var pote = "bombom";
+alert(pote);*/
+
+var a,b,c; //declaração de variaveis sem colocar var em todas
+
+//atribuição 
+a=2;
+b=3;
+c=a+b;
+
+var a =5; //reatribuindo valores
+alert(c);
+
+
--- scripts/idex2.js
+++
@@ -1,5 +0,0 @@
-var PESSOA = "viny";
-var pessoa = "duds"
-alert(`${PESSOA}`)
-
-// pessoa e PESSOA  são clases diferentes pelas as letras maiusculas
