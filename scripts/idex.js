4c7ec80897bc9b0aac020b06b2d8ae3f7a909a42
spark.zz <vinynine2009@gmail.com>
2026-09-03 21:03:59 -0300

    aprendendo a sintaxe do js

---
+++ scripts/idex.js
@@ -0,0 +1,6 @@
+var a = 1;
+var b = 5;
+var c = a + b; 
+alert(`${c}`)
+/* definindo variaveis e printando elas com alert, podendo 
+utlizar outras operações tambem !*/
---
+++ scripts/idex2.js
@@ -0,0 +1,5 @@
+var PESSOA = "viny";
+var pessoa = "duds"
+alert(`${PESSOA}`)
+
+// pessoa e PESSOA  são clases diferentes pelas as letras maiusculas
